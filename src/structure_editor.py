424b661f6a2b31aec7cc7d2c53187fdcf843c05c
gralerfics <glverfer@outlook.com>
2025-03-25 18:44:03 +0800

    bkup.

--- src/structure_editor.py
+++ src/structure_editor.py
@@ -130,6 +130,9 @@ class StructureEditor:
         ed_config.settings_file = ""
         self.context: ed.EditorContext = ed.create_editor(ed_config)
         self.gui_is_first_frame = True
+        
+        # TODO
+        self.m_HeaderBackground: imgui.ImTextureID
     
     def __del__(self):
         ed.destroy_editor(self.context)
@@ -147,14 +150,91 @@ class StructureEditor:
             
             # TODO 遍历 ports_inside_flipped 绘制端口, 遍历 subs 绘制节点 (pin 对应相应 ports_outside), 遍历收集 net 绘制连线.
             
-            with ctx.node() as n:
-                imgui.text(f"Node {n.id.id()}")
+            
+            # <For each node BEGIN>
+            # builder.Begin
+            ed.push_style_var(ed.StyleVar.node_padding, imgui.ImVec4(8, 4, 8, 8)) # builders.cpp: 34
+            with ctx.node() as n: # builders.cpp: 36
+                # imgui.push_id(n.id.id()) # builders.cpp: 38
+                CurrentNodeId = n.id
                 
-                with ctx.pin(ed.PinKind.input) as p:
-                    imgui.text(f"In {p.id.id()}")
-                imgui.same_line()
-                with ctx.pin(ed.PinKind.output) as p:
-                    imgui.text(f"Out {p.id.id()}")
+                # <1> # blueprints-example.cpp: 947
+                # builder.Header
+                HeaderColor = imgui.ImColor(imgui.ImVec4(0.4, 0.4, 0.4, 1.0)) # builders.cpp: 96
+                imgui.begin_horizontal("header") # builders.cpp: 237
+                
+                imgui.spring(0) # blueprints-example.cpp: 950
+                imgui.text_unformatted("Node_name")
+                imgui.spring(1)
+                imgui.dummy(imgui.ImVec2(0, 28))
+                if False: # TODO hasOutputDelegates
+                    pass
+                else:
+                    imgui.spring(0) # blueprints-example.cpp: 990
+                
+                # builder.EndHeader
+                imgui.end_horizontal() # builders.cpp: 176
+                HeaderMin = imgui.get_item_rect_min() # builders.cpp: 177
+                HeaderMax = imgui.get_item_rect_max() # builders.cpp: 178
+                # imgui.spring(0, imgui.get_style().item_spacing.y * 2.0) # builders.cpp: 181
+                # TODO builders.cpp: 240
+                imgui.begin_horizontal("content") # builders.cpp: 244
+                imgui.spring(0, 0)
+                imgui.end_horizontal()
+                
+                # <2> # blueprints-example.cpp: 994
+                # <For each in BEGIN>
+                # alpha = imgui.get_style().alpha # blueprints-example.cpp: 996
+                # alpha *= 48 / 255 # TODO
+                
+                # builder.Input(input_id)
+                imgui.begin_vertical("inputs", imgui.ImVec2(0, 0), 0.0) # builders.cpp: 249
+                ed.push_style_var(ed.StyleVar.pivot_alignment, imgui.ImVec2(0, 0.5))
+                ed.push_style_var(ed.StyleVar.pivot_size, imgui.ImVec2(0, 0))
+                # imgui.spring(0) # builders.cpp: 114
+                with ctx.pin(ed.PinKind.input) as p: # builders.cpp: 117
+                    imgui.begin_horizontal(p.id.id()) # builders.cpp: 119
+                    
+                    imgui.push_style_var(imgui.StyleVar_.alpha, 120) # alpha) # blueprints-example.cpp: 1001
+                    # DrawPinIcon TODO
+                    imgui.spring(0) # blueprints-example.cpp: 1003
+                    if True: # blueprints-example.cpp: 1004
+                        imgui.text_unformatted("Input_name")
+                        imgui.spring(0)
+                    # if Bool TODO
+                    imgui.pop_style_var()
+                    
+                    # builder.EndInput
+                    imgui.end_horizontal() # builders.cpp: 124
+                ed.pop_style_var(2)
+                imgui.spring(1, 0)
+                imgui.end_vertical()
+                
+                # <For each in END>
+                
+                # <3> isSimple = True TODO
+                
+                # <4> # blueprints-example.cpp: 1027 TODO
+                
+                # builder.End
+                # setstage end # builders.cpp: 46
+            # builders.cpp: 48
+            
+            # TODO ImGui::IsItemVisible()
+            drawList = ed.get_node_background_draw_list(CurrentNodeId)
+            halfBorderWidth = ed.get_style().node_border_width * 0.5
+            headerColor = imgui.IM_COL32(100, 100, 100, 120) # imgui.IM_COL32(0, 0, 0, 120) | (HeaderColor & imgui.IM_COL32(255, 255, 255, 0))
+            
+            
+            if HeaderMax.x > HeaderMin.x and HeaderMax.y > HeaderMin.y:
+                pass
+            
+            # imgui.pop_id()
+            CurrentNodeId = 0
+            ed.pop_style_var()
+            
+            # <For each node END>
+            
             
             # with ctx.node() as n:
             #     imgui.text(f"Node {n.id.id()}")
